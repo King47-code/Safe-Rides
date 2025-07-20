3c554e976b9e49ea84367f27165e86d61574f9af
King47-code <131360636+King47-code@users.noreply.github.com>
2025-07-20 13:43:56 +0000

    Update api.js

--- api.js
+++ api.js
@@ -1,7 +1,6 @@
-// src/api.js
 import axios from 'axios';
 
-const API_BASE       = process.env.REACT_APP_API_BASE;
+const API_BASE = process.env.REACT_APP_API_BASE;
 
 // Helper to retrieve stored JWT
 function getToken() {
@@ -23,22 +22,22 @@ export const login = (phone, password) =>
 // Fare estimation
 export const fetchFare = (pickup, dropoff) =>
   axios.post(`${API_BASE}/api/rides/fare`, { pickup, dropoff }, authHeaders())
-       .then(r => r.data);
+    .then(r => r.data);
 
 // Request a ride
 export const requestRide = (pickup, dropoff) =>
   axios.post(`${API_BASE}/api/rides/request`, { pickup, dropoff }, authHeaders())
-       .then(r => r.data);
+    .then(r => r.data);
 
 // Ride history
 export const fetchHistory = () =>
   axios.get(`${API_BASE}/api/rides/history`, authHeaders())
-       .then(r => r.data);
+    .then(r => r.data);
 
-// **Nearby drivers** (added)
+// Nearby drivers
 export const fetchNearbyDrivers = () =>
   axios.get(`${API_BASE}/api/drivers/nearby`, authHeaders())
-       .then(r => r.data);
+    .then(r => r.data);
 
 // Driver location update
 export const updateDriverLocation = (lng, lat) =>
@@ -47,11 +46,11 @@ export const updateDriverLocation = (lng, lat) =>
 // Payments & earnings
 export const fetchPayments = () =>
   axios.get(`${API_BASE}/api/payments`, authHeaders())
-       .then(r => r.data);
+    .then(r => r.data);
 
 export const fetchEarnings = () =>
   axios.get(`${API_BASE}/api/driver/earnings`, authHeaders())
-       .then(r => r.data);
+    .then(r => r.data);
 
 // Real‑time socket.io
 export const initSocket = () => {
